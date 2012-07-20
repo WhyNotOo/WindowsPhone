e8c0002e1b38d2aa42c700521372f7f4f8610894
WhyNotOo <k.albessard@gmail.com>
2012-07-20 16:32:40 +0200

    Add loadmore action

--- app/js/script.js
+++ app/js/script.js
@@ -6,18 +6,25 @@
 	var datasources = Joshfire.factory.getDataSource("various"),
       options     = Joshfire.factory.config.template.options || {},
       icons       = new Array(),
-      allSources  = new Object;
+      sourceBase  = new Object,
+      allSources  = new Object,
+      containerW  = $('#container').width();
 
   icons['Twitter']  = 'img/windows/Twitter.png';
   icons['Flickr']   = 'img/windows/Photoshop.png';
   icons['Instagram']= 'img/windows/Text.png';
+  icons['Blog']     = 'img/windows/Wapedia.png';
 
   var app = {
 
   	getDatasources: function() {
-  		var main = $('.view1 .datasources'), i;
+  		var main = $('.view1 .datasources');
 
       $('.view1').after('<section class="view view2 hidden"><aside class="aside"><img src="img/arrow-back.png" title="See more" class="changeView" /></aside><section class="datasources"></section></section>');
+
+      for(var i = 0, len = datasources.children.length; i < len; i++) {
+        sourceBase[datasources.children[i].name] = i;
+      }
 
       datasources.find({}, function (err, data) {
         if(err) {
@@ -25,23 +32,45 @@
           alert('Une erreur est survenue pendant le chargement de l\'application. Merci de recharger la page.');
         } else {
           $.map(data.entries, function (entry, idx) {
-            allSources['source'+idx] = {
+            allSources[entry.name] = {
               'name'  : entry.name,
               'items' : entry.entries
             };
             var source = entry.entries;
 
             if(idx <= 5) {
-      				main.append('<article class="datasource" data-id="source'+idx+'"><img src="'+icons[entry.name]+'" /><p class="number">'+source.length+'</p><h2>'+entry.name+'</h2></article>');
+      				main.append('<article class="datasource" data-source="'+entry.name+'"><img src="'+icons[entry.name]+'" /><p class="number">'+source.length+'</p><h2>'+entry.name+'</h2></article>');
     	  		} else {
-    	  			$('.view2 .datasources').append('<article class="datasource" data-id="'+idx+'"><img src="'+icons[entry.name]+'" /><p class="number">'+source.length+'</p><h2>'+entry.name+'</h2></article>');
+    	  			$('.view2 .datasources').append('<article class="datasource" data-source="'+entry.name+'"><img src="'+icons[entry.name]+'" /><p class="number">'+source.length+'</p><h2>'+entry.name+'</h2></article>');
     	  			$('.view1 .changeView').removeClass('hidden');
     	  		}
           });
           app.resizeItem();
         }
   		});
+
+      // Place views
+      $('#detailSource, #detailItem').css('left', containerW);
   	},
+
+    getUniqueSource: function(result) {
+      console.log(result);
+    },
+
+    loadMoreEntries: function(source) {
+      $('#detailSource').addClass('loading');
+      $('#loadmore span').addClass('hidden');
+
+      var src = sourceBase[source];
+
+      datasources.children[src].fetch({
+        success: function(result) {
+          $('#detailSource').removeClass('loading');
+          $('#loadmore span').removeClass('hidden');
+          app.getUniqueSource(result);
+        }
+      });
+    },
 
   	displayDatasource: function(source) {
 			var content = allSources[source],
@@ -49,13 +78,24 @@
 
       list.attr('data-name', source);
 
-      if(content.name == "Twitter") {
+      if(content.name == 'Twitter') {
         for(var i = 0, len = content.items.length; i < len; i++) {
-          list.append('<li class="clearfix" data-item="'+i+'"><img class="thumbnail" src="'+content.items[i].author[0].image.contentURL+'" /><p class="title">'+content.items[i].name+'</p></li>');
+          var image = (content.items[i].author[0].image.contentURL) ? content.items[i].author[0].image.contentURL : 'img/noimage.png';
+          list.append('<li class="clearfix" data-item="'+i+'"><img class="thumbnail" src="'+image+'" /><p class="title">'+content.items[i].name+'</p></li>');
+        }
+      } else if(content.name == 'Blog') {
+        for(var i = 0, len = content.items.length; i < len; i++) {
+          if(content.items[i].image.contentURL != undefined)
+            console.log(content.items[i].image.contentURL);
+          else
+            console.log('no image');
+          var image = (content.items[i].image.contentURL) ? content.items[i].image.contentURL : 'img/noimage.png';
+          list.append('<li class="clearfix" data-item="'+i+'"><img class="large" src="'+image+'" /></li>');
         }
       } else {
         for(var i = 0, len = content.items.length; i < len; i++) {
-          list.append('<li class="clearfix" data-item="'+i+'"><img class="large" src="'+content.items[i].contentURL+'" /></li>');
+          var image = (content.items[i].contentURL) ? content.items[i].contentURL : 'img/noimage.png';
+          list.append('<li class="clearfix" data-item="'+i+'"><img class="large" src="'+image+'" /></li>');
         }
       }
 
@@ -63,23 +103,36 @@
   	},
 
   	displayItem: function(source, item) {
-      var content = allSources[source].items[item],
+      var content = allSources[source],
+          item    = content.items[item],
           daddy   = $('#detail'),
-          date    = app.getDate(content.dateCreated);
+          date;
 
-      console.log(content);
+      if(content.name == 'Twitter' || content.name == 'Blog') {
+        date = app.getDate(item.datePublished);
+        if(content.name == 'Twitter') {
+          daddy.children('img').attr({ 'src': item.author[0].image.contentURL, 'title': item.name });
+          daddy.children('.author').html((item.author[0].name).toUpperCase());
+        } else if(content.name == 'Blog') {
+          daddy.children('img').attr({ 'src': item.image.contentURL, 'title': item.name });
+          daddy.children('.author').html((item.author.name).toUpperCase());
+          daddy.children('.text').html(item.articleBody);
+        }
+      } else {
+        date = app.getDate(item.dateCreated);
+        daddy.children('.author').html((item.author[0].name).toUpperCase());
+        daddy.children('img').attr({ 'src': item.contentURL, 'title': item.name });
+      }
 
-      daddy.children('h3').html(content.name);
       daddy.children('.date').html(date);
-      daddy.children('img').attr({ 'src': content.contentURL, 'title': content.name });
-      daddy.children('.author').html(content.author[0].name);
+      daddy.children('h3').html(item.name);
 
       app.slideView('#detailSource', '#detailItem');
   	},
 
-    slideView: function(start, end) {
-      $(start).slideUp();
-      $(end).removeClass('hidden').slideDown();
+    slideView: function(hide, show) {
+      $(hide).animate({ left:'-'+(containerW+40)+'px' }, function() { $(this).addClass('hidden'); });
+      $(show).removeClass('hidden').animate({ left:'4%' });
     },
 
     getDate: function(date) {
@@ -136,7 +189,7 @@
 
 // --- ACTIONS
   $('.datasource').live('click', function() {
-  	app.displayDatasource($(this).attr('data-id'));
+  	app.displayDatasource($(this).attr('data-source'));
 	});
 
   $('#sourceList li').live('click', function() {
@@ -144,6 +197,11 @@
         item    = $(this).attr('data-item');
 
     app.displayItem(source, item);
+  });
+
+  $('#loadmore').click(function() {
+    var source = $('#sourceList ol').attr('data-name');
+    app.loadMoreEntries(source);
   });
 
 // HAMMER JS + inclide js files in html
@@ -170,14 +228,12 @@
 
   $('.backHome').live('click', function() {
     $('#sourceList ol').html('').attr('data-name', '');
-
     app.slideView('#detailSource', '#main');
   });
 
   $('.backList').live('click', function() {
-    $('#detail').children('h3, .date, .desc').html('');
+    $('#detail').children().html('');
     $('#detail').children('img').attr({'title': '', 'src': ''});
-
     app.slideView('#detailItem', '#detailSource');
   });
 
